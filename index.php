364f3db9fcacd0de42cb02143213456b9790d0d7
Maks Cherevko <yourewen@gmail.com>
2023-02-09 15:39:17 +0200

    begin

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <title>QR-generator</title>
+    </head>
+    <body>
+        <main class="main">
+            <div class="main__container">
+                <div class="main__content">
+                    <textarea class="main__text" name="">
+                        
+                    </textarea>
+                    <div class="main__qrcode">
+                        
+                    </div>
+                </div>
+            </div>
+        </main>
+        <script src="js/script.js"></script>
+    </body>
+</html>
